41d58a0f43a70b93f9042ac1cdc2b42cad6a1898
NAUFALIHZA\Naufal Ihza <nauval.ihza@yahoo.com>
2016-10-20 08:29:35 +0700

    bikin bata 10 x 10

---
+++ nrp5216100034.java
@@ -0,0 +1,12 @@
+class nrp5216100034 {
+	public static void main (String[] args){
+	String bata = "[==]" ;
+	for(int i=0; i < 10; i++)	{
+			for(int k=0; k < 9; k++)	{
+			System.out.print(bata);
+			}
+		System.out.println(bata);
+	
+	}
+	}
+}
